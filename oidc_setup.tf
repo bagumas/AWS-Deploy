d2cab32cf1e6ee7bf5167099a155b8b8394c4b29
Samuel Bahuma <kabsam99@gmail.com>
2026-07-26 14:52:58 -0700

    security: streamline OIDC token claim path tracking rules

--- oidc_setup.tf
+++ oidc_setup.tf
@@ -16,10 +16,10 @@ resource "aws_iam_role" "github_actions" {
         Action    = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:bagumas@33612024/AWS-Deploy@1312335209:ref:refs/heads/main"
+            "://githubusercontent.com:sub" = "repo:bagumas@33612024/AWS-Deploy@1312335209:ref:refs/heads/main*"
           }
           StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "://githubusercontent.com:aud" = "://amazonaws.com"
           }
         }
       }
